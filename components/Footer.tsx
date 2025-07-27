51d5ad8c2c8a6fc4eeead867e106dc4920c7204b
Tandra Kole <72182420+Jakilator@users.noreply.github.com>
2025-07-27 20:27:38 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ const Footer: React.FC = () => {
                 </div>
                 <div className="text-sm mt-2">
                     © {new Date().getFullYear()} {NEXT_PUBLIC_SITE_NAME} 
-                    <Link href="https://github.com/rafsanbasunia/reelnn" target="_blank" rel="noopener noreferrer" className="ml-1 underline inline-flex items-center">
+                    <Link href="https://www.dockeeper.eu.org" target="_blank" rel="noopener noreferrer" className="ml-1 underline inline-flex items-center">
                         <FaGithub className="mr-1" aria-hidden="true" />Made with ❤️
                     </Link>
                 </div>
